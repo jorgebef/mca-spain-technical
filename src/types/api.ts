24d64f052978cab98719518fc7bdc1a4f1f43f10
Jorge Befan <jorge.befan@gmail.com>
2024-04-28 19:40:29 +0200

    añadir tipado para la respuesta del track y facilitar trabajo

--- src/types/api.ts
+++ src/types/api.ts
@@ -8,3 +8,16 @@ export type Top100Result = {
 	// basado en la respuesta de la API
 	[k: string]: any;
 };
+
+export type TrackResult = {
+	trackId: string;
+	episodeUrl: string;
+	trackTimeMillis: number;
+	releaseDate: string;
+	description: string;
+	trackName: string;
+	// Para dar la oportunidad de acceder a otras propiedades
+	// TODO eliminarlo y completar con el tipado correcto
+	// basado en la respuesta de la API
+	[k: string]: any;
+};
