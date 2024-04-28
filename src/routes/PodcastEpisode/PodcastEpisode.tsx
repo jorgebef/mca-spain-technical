7fb24eba4c163ff3ec4ff117200d0b2b151afe22
Jorge Befan <jorge.befan@gmail.com>
2024-04-28 19:40:48 +0200

    añadir parte final para la vista del episodio de un podcast

--- src/routes/PodcastEpisode/PodcastEpisode.tsx
+++ src/routes/PodcastEpisode/PodcastEpisode.tsx
@@ -1,27 +1,50 @@
 import { useParams } from "react-router-dom";
-import { fetchEpisodeById } from "../../lib/api";
+import { fetchPodcastTracks } from "../../lib/api";
 import { useQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { TrackResult } from "../../types/api";
+import styles from "./PodcastEpisode.module.css";
 
 export const PodcastEpisode = () => {
-	const { episodeId } = useParams();
+	const { podcastId, episodeId } = useParams();
+
+	const [episodeData, setEpisodeData] = useState<TrackResult | undefined>(
+		undefined,
+	);
+
+	const testDescription = "kekekeke <br/> <div>lulululul</div>";
 
 	const { data, error, isLoading } = useQuery({
-		queryKey: ["episode", episodeId],
-		queryFn: () => fetchEpisodeById(episodeId),
+		queryKey: ["trackList", podcastId],
+		queryFn: () => fetchPodcastTracks(podcastId),
+		// queryFn: () =>
+		// 	fetchPodcastTracks(podcastId).then((r) =>
+		// 		r.find((e: any) => e.trackId === episodeId),
+		// 	),
 	});
+
 	useEffect(() => {
+		if (data === undefined || error || isLoading) return;
 		console.log("Episode: ");
-		console.log(data);
+		const selectedEpisode = data.find(
+			(e: TrackResult) => String(e.trackId) === episodeId,
+		);
+		console.log(selectedEpisode);
+
+		setEpisodeData(selectedEpisode);
 	}, [error, data]);
 
 	if (error) return <div>Error!!</div>;
 	if (isLoading) return <div>Loading...</div>;
-	if (data === undefined) return <div>No data</div>;
+	if (episodeData === undefined) return <div>No data</div>;
 
 	return (
-		<div>
-			<div>Episode {episodeId}</div>
+		<div className={styles.container}>
+			<div className={styles.info}>
+				<h1>{episodeData.trackName}</h1>
+				<p dangerouslySetInnerHTML={{ __html: episodeData.description }} />
+				<audio controls src={episodeData.episodeUrl}></audio>
+			</div>
 		</div>
 	);
 };
