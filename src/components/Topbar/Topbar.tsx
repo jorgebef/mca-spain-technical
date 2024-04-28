fa2b9d9d9954587c343e6f6410d4831120f1916f
Jorge Befan <jorge.befan@gmail.com>
2024-04-28 20:18:23 +0200

    detectar todos los estados pending de react query en toda la app

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
+// import { useState } from "react";
 import style from "./Topbar.module.css";
 import { Link } from "react-router-dom";
+import { useIsFetching } from "@tanstack/react-query";
 export const Topbar = () => {
-	const [pending, setPending] = useState(false);
+	const pending = useIsFetching();
 
 	return (
 		<div className={style.topbar}>
 			<Link to="/">Podcaster</Link>
-			{pending && <div className={style.indicator}></div>}
+			{pending ? <div className={style.indicator}></div> : null}
 		</div>
 	);
 };
