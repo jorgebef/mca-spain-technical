8c3f763fcd522a71a7052a022e1d241302225020
Jorge Befan <jorge.befan@gmail.com>
2024-04-28 19:50:18 +0200

    eliminar código innecesario y checkear que haya data para mostrar

--- src/routes/PodcastEpisode/PodcastEpisode.tsx
+++ src/routes/PodcastEpisode/PodcastEpisode.tsx
@@ -12,31 +12,23 @@ export const PodcastEpisode = () => {
 		undefined,
 	);
 
-	const testDescription = "kekekeke <br/> <div>lulululul</div>";
-
 	const { data, error, isLoading } = useQuery({
 		queryKey: ["trackList", podcastId],
 		queryFn: () => fetchPodcastTracks(podcastId),
-		// queryFn: () =>
-		// 	fetchPodcastTracks(podcastId).then((r) =>
-		// 		r.find((e: any) => e.trackId === episodeId),
-		// 	),
 	});
 
 	useEffect(() => {
 		if (data === undefined || error || isLoading) return;
-		console.log("Episode: ");
 		const selectedEpisode = data.find(
 			(e: TrackResult) => String(e.trackId) === episodeId,
 		);
-		console.log(selectedEpisode);
-
 		setEpisodeData(selectedEpisode);
-	}, [error, data]);
+	}, [error, isLoading, data]);
 
 	if (error) return <div>Error!!</div>;
 	if (isLoading) return <div>Loading...</div>;
-	if (episodeData === undefined) return <div>No data</div>;
+	if (episodeData === undefined || data === undefined)
+		return <div>No data</div>;
 
 	return (
 		<div className={styles.container}>
